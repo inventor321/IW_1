8a57a81c99ba1db2cf20e855ef4d65a11da3abe2
gillermillones <titicht@hotmail.com>
2025-02-18 13:57:01 +0100

    org

--- plantilla/src/main/java/es/ucm/fdi/iw/model/EventService.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/model/EventService.java
@@ -11,36 +11,36 @@ import java.util.List;
 
 @Service
 public class EventService {
-    
+
     @Autowired
     private EventRepository eventRepository;
-    
+
     @PostConstruct
     public void init() {
         // Add sample data if repository is empty
         if (eventRepository.count() == 0) {
             Event event1 = new Event("Concurso de tortillas de patatas",
-                "Descripción del evento 1",
-                LocalDateTime.of(2025, 2, 21, 12, 0),
-                "Lugar 1",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Tortilla_de_patatas.jpg/640px-Tortilla_de_patatas.jpg"
-            );
+                    "Descripción del evento 1",
+                    LocalDateTime.of(2025, 2, 21, 12, 0),
+                    "Lugar 1",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Tortilla_de_patatas.jpg/640px-Tortilla_de_patatas.jpg",
+                    1);
             eventRepository.save(event1);
 
             Event event2 = new Event("Concurso de tortillas de patatas 2",
-                 "Descripción del evento 2",
-                LocalDateTime.of(2025, 2, 28, 12, 0),
-                "Lugar 1",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Tortilla_de_patatas.jpg/640px-Tortilla_de_patatas.jpg"
-            );
+                    "Descripción del evento 2",
+                    LocalDateTime.of(2025, 2, 28, 12, 0),
+                    "Lugar 2",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Tortilla_de_patatas.jpg/640px-Tortilla_de_patatas.jpg",
+                    1);
             eventRepository.save(event2);
         }
     }
-    
+
     public List<Event> findAll() {
         return eventRepository.findAllByOrderByDateAsc();
     }
-    
+
     public Event save(Event event) {
         return eventRepository.save(event);
     }
