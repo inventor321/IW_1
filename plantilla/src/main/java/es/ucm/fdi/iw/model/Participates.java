24e39c6a4901a4551f22728d3fa59f9a99dcf3cb
anonymous <marcoapr@ucm.es>
2025-03-17 15:24:59 +0300

    fixed join table + event attribute on message

--- plantilla/src/main/java/es/ucm/fdi/iw/model/Participates.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/model/Participates.java
@@ -1,35 +1,57 @@
 package es.ucm.fdi.iw.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "participates")
 public class Participates {
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long idGroup;
-    private Long idUser;
+
+    @ManyToOne
+    @JoinColumn(name = "id_event", nullable=false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_group", nullable=false)
+    private UserGroup userGroup;
 
     public Participates() {
     }
 
-    public Participates(Long id, Long idUser) {
-        this.id = id;
-        this.idUser = idUser;
+    public Participates(User user, UserGroup userGroup) {
+        this.user = user;
+        this.userGroup = userGroup;
+       
     }
 
-    public Long getId() { return id; }
+    public Long getId() {
+        return id;
+    }   
 
-    public void setId(Long id) { this.id = id; }    
+    public User getUser() {
+        return user;
+    }   
 
-    public Long getIdUser() { return idUser; }
+    public UserGroup getUserGroup() {
+        return userGroup;
+    }
 
-    public void setIdUser(Long idUser) { this.idUser = idUser; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setUserGroup(UserGroup userGroup) {
+        this.userGroup = userGroup;
+    }
 
 }
