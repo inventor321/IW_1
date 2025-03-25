1636702bb51d8c07d30ff4e146b62c6b67afc9fb
Allen Pin <all32189000@gmail.com>
2025-03-25 14:00:11 +0100

    rename buscar imagen

--- plantilla/src/main/java/es/ucm/fdi/iw/model/EventService.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/model/EventService.java
@@ -18,23 +18,23 @@ public class EventService {
     @PostConstruct
     public void init() {
         // Add sample data if repository is empty
-        if (eventRepository.count() == 0) {
-            Event event1 = new Event("Concurso de tortillas de patatas",
-                    "Descripción del evento 1",
-                    LocalDateTime.of(2025, 2, 21, 12, 0),
-                    "Lugar 1",
-                    "/img/event1.jpg", 
-                    (long) 1);
-            eventRepository.save(event1);
+        // if (eventRepository.count() == 0) {
+        //     Event event1 = new Event("Concurso de tortillas de patatas",
+        //             "Descripción del evento 1",
+        //             LocalDateTime.of(2025, 2, 21, 12, 0),
+        //             "Lugar 1",
+        //             "/img/event1.jpg", 
+        //             (long) 1);
+        //     eventRepository.save(event1);
 
-            Event event2 = new Event("Concurso de tortillas de patatas 2",
-                    "Descripción del evento 2",
-                    LocalDateTime.of(2025, 2, 28, 12, 0),
-                    "Lugar 2",
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Tortilla_de_patatas.jpg/640px-Tortilla_de_patatas.jpg",
-                    (long) 1);
-            eventRepository.save(event2);
-        }
+        //     Event event2 = new Event("Concurso de tortillas de patatas 2",
+        //             "Descripción del evento 2",
+        //             LocalDateTime.of(2025, 2, 28, 12, 0),
+        //             "Lugar 2",
+        //             "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Tortilla_de_patatas.jpg/640px-Tortilla_de_patatas.jpg",
+        //             (long) 1);
+        //     eventRepository.save(event2);
+        // }
     }
 
     public List<Event> findAll() {
