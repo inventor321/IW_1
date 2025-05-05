31d55d24545e7c8aa5979f4feda08239dd7653c2
aracavic <aracamatelmo@gmail.com>
2025-05-05 14:42:14 +0200

    cambios varios

--- plantilla/src/main/java/es/ucm/fdi/iw/repository/EventRepository.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/repository/EventRepository.java
@@ -3,10 +3,28 @@ package es.ucm.fdi.iw.repository;
 import es.ucm.fdi.iw.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     // Ordena todos los eventos por fecha en orden ascendente
     List<Event> findAllByOrderByDateAsc();
+    List<Event> findByCategory(Event.Category category);
+    List<Event> findByDate(LocalDateTime date);
+    List<Event> findByNameContaining(String name);
+    List<Event> findAllByActiveTrueOrderByDateAsc(); // Busca solo eventos habilitados y los ordena por fecha
+    /*List<Event> findByNameContainingIgnoreCase(String query);
+    List<Event> findByLocationContainingIgnoreCase(String query);
+    List<Event> findByCategoryContainingIgnoreCase(String query);
+    */
+
+    @Modifying
+    @Query("UPDATE Event e SET e.active = false WHERE e.id = :id")
+    void disableEventById(@Param("id") Long id);
 }
