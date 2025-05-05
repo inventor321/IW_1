60ed34f05b34f0afd9c08bb2a876609792e5ab6d
gillermillones <titicht@hotmail.com>
2025-05-05 14:44:18 +0200

    en proceso
    el calkendario

--- plantilla/src/main/resources/static/js/calendario.js
+++ plantilla/src/main/resources/static/js/calendario.js
@@ -14,64 +14,14 @@ document.addEventListener('DOMContentLoaded', function() {
       selectMirror: true,
       dayMaxEvents: true, // allow "more" link when too many events
       events: [
-        {
-          title: 'All Day Event',
-          start: '2023-01-01',
-        },
-        {
-          title: 'Long Event',
-          start: '2023-01-07',
-          end: '2023-01-10'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2023-01-09T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2023-01-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2023-01-11',
-          end: '2023-01-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-01-12T10:30:00',
-          end: '2023-01-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2023-01-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-01-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2023-01-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2023-01-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2023-01-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2025-05-05'
-        }
       ]
     };
 
     var calendar2 = new FullCalendar.Calendar(document.getElementById('calendar2'), calendarOptions);
+    calendar2.addEvent({
+      title: 'Second Event',
+      start: '2025-05-08T12:30:00'
+    })
     calendar2.render();
 
     /*
