dcad9acb9dee040c3b91eefecff52d7642c20b2b
aracavic <aracamatelmo@gmail.com>
2025-05-10 15:26:28 +0200

    error

---
+++ target/classes/import.sql
@@ -0,0 +1,14 @@
+-- insert admin (username a, password aa)
+INSERT INTO IWUser (id, enabled, roles, username, password)
+VALUES (1, TRUE, 'ADMIN,ORG,USER', 'ADMIN',
+    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
+
+INSERT INTO IWUser (id, enabled, roles, username, password)
+VALUES (2, TRUE, 'ORG,USER', 'ORG',
+    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
+INSERT INTO IWUser (id, enabled, roles, username, password)
+VALUES (3, TRUE, 'USER', 'USER',
+    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
+
+-- start id numbering from a value that is larger than any assigned above
+ALTER SEQUENCE "PUBLIC"."GEN" RESTART WITH 1024;
---
+++ target/classes/static/js/calendario.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarOptions = {
+      initialDate: '2025-05-05',
+      initialView: 'timeGridWeek',
+      nowIndicator: true,
+      headerToolbar: {
+        left: 'prev, next, today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      selectable: true,
+      selectMirror: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      titleFormat: '',
+      events: [
+      ]
+    };
+
+    var calendar2 = new FullCalendar.Calendar(document.getElementById('calendar2'), calendarOptions);
+    for(i = 0; i < lEventosJS.length; i++){
+      calendar2.addEvent({
+        title: lEventosJS[i].name,
+        start: lEventosJS[i].date,
+        end: lEventosJS[i].ending,
+        url: '/events/' + lEventosJS[i].id
+      });
+    }
+    calendar2.render();
+
+    /*
+    Modal
+    */
+
+    var modal = document.querySelector('.modal');
+    var modalTrigger = document.querySelector('.modal-trigger');
+    var modalOverlay = document.querySelector('.modal-overlay');
+
+    modalTrigger.addEventListener('click', function() {
+      modal.classList.add('is-visible');
+      calendar2.updateSize();
+    });
+    modalOverlay.addEventListener('click', function() {
+      modal.classList.remove('is-visible');
+    });
+  });
